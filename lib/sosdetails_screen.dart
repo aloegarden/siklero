a4e7ae5e6ad7fb22aff60c06e027d4e9494c6210
aloegarden <yangadaryl8@gmail.com>
2022-10-31 15:51:47 +0800

    Added placeholder widget

--- lib/sosdetails_screen.dart
+++ lib/sosdetails_screen.dart
@@ -1,15 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:siklero/model/user_info.dart';
 
 class SOSDetailsScreen extends StatefulWidget {
-  const SOSDetailsScreen({super.key});
+  const SOSDetailsScreen({super.key, UserData? userInfo});
 
   @override
   State<SOSDetailsScreen> createState() => _SOSDetailsScreenState();
 }
 
 class _SOSDetailsScreenState extends State<SOSDetailsScreen> {
+
+  final user = FirebaseAuth.instance.currentUser!;
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("SOS Details"),
+      ),
+
+      body: Center(child: Text(user.uid),),
+    );
   }
 }

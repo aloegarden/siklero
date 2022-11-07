72d2e6af982b6ca0099238d0b49dadc5be83b157
aloegarden <yangadaryl8@gmail.com>
2022-11-07 19:00:07 +0800

    Added UI, Backend Functionalities

--- lib/user/soscall_screen.dart
+++ lib/user/soscall_screen.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:siklero/main.dart';
+import 'package:siklero/user/home_screen.dart';
 
 class SOSCallScreen extends StatefulWidget {
   final String sosID;
@@ -12,35 +14,50 @@ class SOSCallScreen extends StatefulWidget {
 class _SOSCallScreenState extends State<SOSCallScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xffed8f5b),
-        title: Text('SOS Call', style: TextStyle(fontFamily: 'OpenSans', fontSize: 24),),
-        centerTitle: true,
-        
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            updateSOSCall();
-            Navigator.pop(context);
-          },
-          child: Text(
-            'Cancel SOS',
-            style: TextStyle(
-              fontFamily: 'OpenSans',
-              fontWeight: FontWeight.w700,
-              fontSize: 40
-            ),),
-          style: ElevatedButton.styleFrom(
-            shape: CircleBorder(),
-            padding: EdgeInsets.all(24),
-            minimumSize: Size(300, 300),
-            backgroundColor: Color(0xffE45F1E),
-            foregroundColor: Colors.white
+    return WillPopScope(
+      onWillPop: () async {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text('Press \'Cancel SOS\' to return to Home Screen')));
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color(0xffed8f5b),
+          title: const Text('SOS Call', style: TextStyle(fontFamily: 'OpenSans', fontSize: 24),),
+          centerTitle: true,
+          
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              updateSOSCall();
+              Navigator.of(context).pushAndRemoveUntil(
+              MaterialPageRoute(
+              builder:(context) => const HomeScreen(),
+
+              ), 
+              (route) => false
+            );
+
+            navigatorKey.currentState!.popUntil((route) => route.isFirst);
+            },
+            style: ElevatedButton.styleFrom(
+              shape: CircleBorder(),
+              padding: EdgeInsets.all(24),
+              minimumSize: Size(300, 300),
+              backgroundColor: Color(0xffE45F1E),
+              foregroundColor: Colors.white
+            ),
+            child: const Text(
+              'Cancel SOS',
+              style: TextStyle(
+                fontFamily: 'OpenSans',
+                fontWeight: FontWeight.w700,
+                fontSize: 40
+              ),),
           ),
-        ),
-        ),
+          ),
+      ),
     );
   }
 
--- lib/user/sosdetails_screen.dart
+++ lib/user/sosdetails_screen.dart
@@ -1,7 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:siklero/main.dart';
+import 'package:siklero/model/sos.dart';
 import 'package:siklero/model/user_info.dart';
+import 'package:siklero/user/soscall_screen.dart';
+import 'package:siklero/user/utils.dart';
 
 class SOSDetailsScreen extends StatefulWidget {
   final UserData? userInfo;
@@ -14,14 +19,15 @@ class SOSDetailsScreen extends StatefulWidget {
 class _SOSDetailsScreenState extends State<SOSDetailsScreen> {
 
   final user = FirebaseAuth.instance.currentUser!;
+  SOSCall sosCall = SOSCall();
+  final formKey = GlobalKey<FormState>();
   TextEditingController detailsController = TextEditingController();
 
   loadData () {
     getCurrentPosition().then((value) async {
-            print(value.latitude.toString() + " " + value.longitude.toString());
-            setState(() {
-              
-            });
+            //print(value.latitude.toString() + " " + value.longitude.toString());
+            sosCall.coordinates = GeoPoint(value.latitude, value.longitude);
+            //print("${sosCall.coordinates?.latitude}  ${sosCall.coordinates?.longitude}");
           });
   }
 
@@ -36,6 +42,37 @@ class _SOSDetailsScreenState extends State<SOSDetailsScreen> {
     return await Geolocator.getCurrentPosition();
   }
 
+  Future writeSOS() async {
+    final isValid = formKey.currentState!.validate();
+    if (!isValid) return;
+
+    DateTime date = DateTime.now();
+
+    sosCall.userID = user.uid;
+    sosCall.details = detailsController.text;
+    sosCall.isActive = true;
+    sosCall.createdAt = Timestamp.fromDate(date);
+
+    try {
+
+      final docSOS = FirebaseFirestore.instance.collection('sos_call').doc();
+      final json = sosCall.toJSON();
+      await docSOS.set(json);
+      
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(
+            builder:(context) => SOSCallScreen(sosID: docSOS.id,),
+          ), 
+          (route) => false
+        );
+  } on FirebaseAuthException catch (e) {
+      Utils.showSnackBar(e.message);
+  }
+
+  navigatorKey.currentState!.popUntil((route) => route.isFirst);
+
+  }
+
   @override
   void initState() {
     super.initState();
@@ -45,61 +82,85 @@ class _SOSDetailsScreenState extends State<SOSDetailsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xffed8f5b),
-        title: const Text('SOS Details', style: TextStyle(fontFamily: 'OpenSans', fontSize: 24),),
-        centerTitle: true,
-      ),
-
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 100),
-        child: Container(
-          decoration: BoxDecoration(
-            color: const Color(0xffFFD4BC),
-            borderRadius: BorderRadius.circular(30)
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(top: 20),
-                child: Image(
-                  //alignment: Alignment.topCenter,
-                  height: 65,
-                  width: 85,
-                  image: AssetImage('asset/img/repair-icon.png'),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          backgroundColor: const Color(0xffed8f5b),
+          title: const Text('SOS Details', style: TextStyle(fontFamily: 'OpenSans', fontSize: 24),),
+          centerTitle: true,
+        ),
+    
+        body: Form(
+          key: formKey,
+          child: Padding(
+            padding: EdgeInsets.all(30),
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xffFFD4BC),
+                borderRadius: BorderRadius.circular(30)
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Column(
+                  //crossAxisAlignment: CrossAxisAlignment.center,
+                  //mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Padding(
+                      padding: EdgeInsets.only(top: 20),
+                      child: Image(
+                        //alignment: Alignment.topCenter,
+                        height: 65,
+                        width: 85,
+                        image: AssetImage('asset/img/repair-icon.png'),
+                      ),
+                    ),
+                    const SizedBox(height: 20,),
+                    const Text("Bicycle Failure Details:", style: TextStyle(
+                      fontSize: 24, 
+                      fontWeight: FontWeight.w700, 
+                      fontFamily: 'OpenSans', 
+                      color: Color(0xff581D00))
+                    ),
+                    const SizedBox(height: 20,),
+                    TextFormField(
+                      maxLines: 3,
+                      maxLength: 50,
+                      style: const TextStyle(fontSize: 20, fontFamily: 'OpenSans'),
+                      controller: detailsController,
+                      decoration: InputDecoration(
+                        hintText: "Type the specified bicycle failure/s encountered.)",
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0)
+                        )
+                      ),
+                      textInputAction: TextInputAction.done,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator:(value) => value != null && value.isEmpty
+                        ? "Don't leave this field empty"
+                        : null
+                    ),
+                    const SizedBox(height: 50,),
+                    ElevatedButton(
+                      onPressed:() => writeSOS(), 
+                      style: ElevatedButton.styleFrom(
+                      shape: StadiumBorder(),
+                      foregroundColor: Colors.white,
+                      backgroundColor: Color(0xffe45f1e),
+                      padding: EdgeInsets.all(30)
+                      ),
+                      child: const Text(
+                        'Confirm SOS Call',
+                        style: 
+                          TextStyle(fontFamily: 'OpenSans', fontSize: 24, fontWeight: FontWeight.w700),
+                    ))
+                  ],
                 ),
               ),
-              Text("Bicycle Failure Details:", style: TextStyle(
-                fontSize: 24, 
-                fontWeight: FontWeight.w700, 
-                fontFamily: 'OpenSans', 
-                color: Color(0xff581D00))
-              ),
-              TextFormField(
-                controller: detailsController,
-                decoration: const InputDecoration(
-                  hintText: "Type the specified bicycle failure/s encountered.)"
-                ),
-                textInputAction: TextInputAction.done,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  print("hello");
-                }, 
-                style: ElevatedButton.styleFrom(
-                shape: const StadiumBorder(),
-                foregroundColor: Colors.white,
-                backgroundColor: const Color(0xffe45f1e)
-                ),
-                child: const Text(
-                  'Confirm SOS Call',
-                  style: 
-                    TextStyle(fontFamily: 'OpenSans', fontSize: 24, fontWeight: FontWeight.w700),
-              ))
-            ],
+            ),
           ),
         ),
       ),

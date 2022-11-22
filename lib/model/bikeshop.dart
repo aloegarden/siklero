0832ea2c4ce3f2d9729eddb47f2dd1685fbed056
aloegarden <yangadaryl8@gmail.com>
2022-11-22 09:32:24 +0800

    Updated model fields

--- lib/model/bikeshop.dart
+++ lib/model/bikeshop.dart
@@ -1,17 +1,29 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-
 class BikeShop {
-  String businessStatus;
-  GeoPoint geometry;
-  String name;
-  String placeId;
-  String vicinity;
+  String? businessStatus;
+  //GeoPoint? geoPoint;
+  double? longitude;
+  double? latitude;
+  String? name;
+  String? placeId;
+  String? vicinity;
 
   BikeShop({
-    required this.businessStatus,
-    required this.geometry,
-    required this.name,
-    required this.placeId,
-    required this.vicinity
+    this.businessStatus,
+    //this.geoPoint,
+    this.longitude,
+    this.latitude,
+    this.name,
+    this.placeId,
+    this.vicinity
   });
+
+  static BikeShop fromJSON(Map<String, dynamic> json) => BikeShop(
+    businessStatus: json['business_status'], 
+    //geoPoint: json['location'],
+    longitude: json['geometry']['location']['lng'],
+    latitude: json['geometry']['location']['lat'], 
+    name: json['name'], 
+    placeId: json['place_id'], 
+    vicinity: json['vicinity']);
 }

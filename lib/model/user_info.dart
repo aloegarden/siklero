cd67f638b01c1c8124adf160c49567e412e6ae18
aloegarden <yangadaryl8@gmail.com>
2022-12-14 13:09:28 +0800

    Updated object field

--- lib/model/user_info.dart
+++ lib/model/user_info.dart
@@ -5,6 +5,8 @@ class UserData {
     late String? address;
     late String? city;
     late String? contact;
+    late String? emergencycontactName;
+    late String? emergencycontactNumber;
     late String? role;
 
     UserData({
@@ -14,12 +16,16 @@ class UserData {
       this.lName,
       this.city,
       this.contact,
+      this.emergencycontactName,
+      this.emergencycontactNumber,
       this.role
     });
 
     Map<String, dynamic> toJson () => {
                 'address': address,
                 'contact': contact,
+                'emergency_contact_name' : emergencycontactName,
+                'emergency_contact_number' : emergencycontactNumber,
                 'first_name': fName,
                 'last_name': lName,
                 'city': city,
@@ -30,6 +36,8 @@ class UserData {
     static UserData fromJSON(Map<String, dynamic> json) => UserData(
         address: json['address'],
         contact: json['contact'],
+        emergencycontactName: json['emergency_contact_name'],
+        emergencycontactNumber: json['emergency_contact_number'],
         fName: json['first_name'],
         lName: json['last_name'],
         city: json['city'],

80293c99ce7619256e862c349f59024d54472346
aloegarden <yangadaryl8@gmail.com>
2023-01-27 14:54:38 +0800

    Removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       scaffoldMessengerKey: Utils.messengerKey,
       navigatorKey: navigatorKey,
       title: 'Siklero',
